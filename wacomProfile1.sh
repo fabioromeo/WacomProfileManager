b2652922c1224d5cbcdc3aab8d88c00a9d5000a4
Fabio Romeo <fabioromeo@users.noreply.github.com>
2019-03-19 13:48:49 -0300

    base profile script

---
+++ wacomProfile1.sh
@@ -0,0 +1,11 @@
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 1 key "1" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 2 key "2" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 3 key "3" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 8 key "8" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" AbsWheelUp "" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 13 "key ctrl shift alt F9" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" AbsWheelDown "" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 9 key "9" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 10 key "0" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 11 key "1" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 12 key "4"
