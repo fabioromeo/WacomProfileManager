152f81fa2d601b0d175c9e410fd1bb01e94bdf2e
Fabio Romeo <fabioromeo@users.noreply.github.com>
2019-03-21 14:53:37 -0300

    updates key shortcut

--- wacomProfile4.sh
+++ wacomProfile4.sh
@@ -3,7 +3,7 @@ xsetwacom set "Wacom Intuos Pro M Pad pad" Button 2 key "2" &&
 xsetwacom set "Wacom Intuos Pro M Pad pad" Button 3 key "3" &&
 xsetwacom set "Wacom Intuos Pro M Pad pad" Button 8 key "8" &&
 xsetwacom set "Wacom Intuos Pro M Pad pad" AbsWheelUp "" &&
-xsetwacom set "Wacom Intuos Pro M Pad pad" Button 13 "key ctrl shift alt F9" &&
+xsetwacom set "Wacom Intuos Pro M Pad pad" Button 13 key "super z" &&
 xsetwacom set "Wacom Intuos Pro M Pad pad" AbsWheelDown "" &&
 xsetwacom set "Wacom Intuos Pro M Pad pad" Button 9 key "9" &&
 xsetwacom set "Wacom Intuos Pro M Pad pad" Button 10 key "0" &&
